0b1e7b8dc5839efd6d63506245ec41f30633f1ca
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-04-11 14:53:52 +0200

    Added map and first assets

---
+++ GraKulka/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MapGenerator : EditorWindow
+{
+	float mapSize = 1f;
+	string generateButton = "Generate map";
+	bool generating = false;
+	string status = "Idle";
+
+	void OnGUI()
+	{
+		GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+		mapSize = EditorGUILayout.Slider("Map size", mapSize, 0, 50);
+		if (GUILayout.Button(generateButton))
+		{
+			if (generating)  //recording
+			{
+				status = "Generating";
+				generateButton = "Generate map";
+				generating = false;
+			}
+			else     // idle
+			{
+				generateButton = "Stop";
+				generating = true;
+			}
+		}
+		EditorGUILayout.LabelField("Status: ", status);
+	}
+
+	void Update()
+	{
+		if (generating)
+		{
+			GenerateMap();
+		}
+	}
+
+	void GenerateMap()
+	{
+
+	}
+}
---
+++ GraKulka/Assets/Scripts/Tools.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Tools
+{
+	[MenuItem("Tools/Assign Tile Material")]
+	public static void AssignTileMaterial()
+	{
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+		Material material = Resources.Load<Material>("Tile");
+
+		foreach (GameObject tile in tiles)
+		{
+			tile.GetComponent<Renderer>().material = material;
+		}
+	}
+
+	[MenuItem("Tools/Assign Tile Script")]
+	public static void AssignTileScript()
+	{
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+		foreach (GameObject tile in tiles)
+		{
+			tile.AddComponent<Tile>();
+		}
+	}
+
+	[MenuItem("Tools/Map Generator")]
+	public static void MapGenerator()
+	{
+		EditorWindow.GetWindow(typeof(MapGenerator));
+	}
+}
