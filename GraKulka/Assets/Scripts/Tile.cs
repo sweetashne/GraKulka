56f112732144d007e553827a6bb37c75d1875fa2
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-04-13 20:37:17 +0200

    Fixed ramp collider

--- GraKulka/Assets/Scripts/Tile.cs
+++ GraKulka/Assets/Scripts/Tile.cs
@@ -22,38 +22,31 @@ public class Tile : MonoBehaviour
 	// MonoBehaviour method. OnGUI is called for rendering and handling GUI events.
 	private void OnGUI()
 	{
-		if (tileSelected)
+		RaycastHit hit;
+
+		// Checks if there is object above the tile and the tile is selected
+		if (tileSelected && !Physics.Raycast(this.transform.position, Vector3.up, out hit, 1))
 		{
 			GUILayout.Label("Add some shit");
-			RaycastHit hit;
+
 			// If user clicked the button.
 			if (GUILayout.Button("Add a wall"))
 			{
-				// Does the ray intersect any objects excluding the player layer
-				if (!Physics.Raycast(this.transform.position, Vector3.up, out hit, 1))
-				{
-					AddWall(this);
-					tileSelected = false;
-				}
+				AddWall(this);
+				tileSelected = false;
 			}
 
 			if (GUILayout.Button("Add a ramp"))
 			{
-				if (!Physics.Raycast(this.transform.position, Vector3.up, out hit, 1))
-				{
-					AddRamp(this);
-					tileSelected = false;
-				}
+				AddRamp(this);
+				tileSelected = false;
 			}
 
 			if (GUILayout.Button("Add a 45 degree wall"))
 			{
 				// Does the ray intersect any objects excluding the player layer
-				if (!Physics.Raycast(this.transform.position, Vector3.up, out hit, 1))
-				{
-					Add45Wall(this);
-					tileSelected = false;
-				}
+				Add45Wall(this);
+				tileSelected = false;
 			}
 		}
 	}
@@ -63,6 +56,8 @@ public class Tile : MonoBehaviour
 		// Create a new object that's loaded from Resources folder.
 		GameObject Ramp = Resources.Load("Prefabs/Wall") as GameObject;
 		Ramp.name = "Ramp";
+		Ramp.GetComponent<BoxCollider>().size = new Vector3(1,1,3);
+		Ramp.GetComponent<BoxCollider>().center = new Vector3(0,0,1);
 		Ramp.transform.localScale = new Vector3(1, 1, 0.1f);
 		Vector3 position = tile.transform.position + new Vector3(0, 0.9f, -0.2f);
 
@@ -75,7 +70,7 @@ public class Tile : MonoBehaviour
 		GameObject WallDegree = Resources.Load("Prefabs/Wall") as GameObject;
 		WallDegree.name = "45Wall";
 		WallDegree.GetComponent<BoxCollider>().size = new Vector3(0.3f, 1, 10);
-		WallDegree.GetComponent<BoxCollider>().center = new Vector3(0, 0, 0);
+		WallDegree.GetComponent<BoxCollider>().center = Vector3.zero;
 		WallDegree.transform.localScale = new Vector3(1.314f, 1, 0.1f);
 		Vector3 position = tile.transform.position + new Vector3(0, 1, 0);
 		Instantiate(WallDegree, position, Quaternion.Euler(new Vector3(0, 45, 0)));
@@ -100,11 +95,5 @@ public class Tile : MonoBehaviour
 		Wall.transform.localScale = new Vector3(1, 1, 0.1f);
 		Vector3 position = tile.transform.position + new Vector3(0, 1, -0.5f);
 		Instantiate(Wall, position, Quaternion.identity);
-	}
-
-	// Checks if there is a wall already on this tile.
-	private void WallCheck(Tile tile)
-	{
-
 	}
 }
