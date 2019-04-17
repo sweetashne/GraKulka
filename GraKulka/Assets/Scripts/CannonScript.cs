ae4ad8f1f4917a3c1e16888e2a9ab4cb5ba7203b
Piotr Rutkowski <piorutko@gmail.com>
2019-04-17 16:38:23 +0200

    try to do a cannon

---
+++ GraKulka/Assets/Scripts/CannonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    private GameObject _cannonBall;
+
+    private float rateOfFire = 0.5f;
+    private float fireDelay = 0.5f;
+    private float fireSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > fireDelay)
+        {
+            fireDelay = Time.time + rateOfFire;
+            GameObject clone = Instantiate(_cannonBall, transform.position, transform.rotation);
+        }
+    }
+}
