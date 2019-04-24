c097678add4a1cc290f176d62c6f5188a6c346db
DESKTOP-85KH1M3\mario <mario2114@gmail.com>
2019-04-24 20:39:44 +0200

    Added trampoline

---
+++ GraKulka/Assets/Launcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+	public float height;
+
+	public GameObject Player;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Player.GetComponent<Rigidbody>().velocity = new Vector3(5, 5, 0);
+			Player.GetComponent<Rigidbody>().AddForce(new Vector3(0, height * Time.deltaTime, 0));
+			
+		}
+	}
+}
