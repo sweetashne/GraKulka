ded4ff5a927c955a15f371133ae588df5b57ae55
Sweetashne <mario2114@gmail.com>
2019-05-13 21:27:50 +0200

    Added ramp sprites

--- GraKulka/Assets/Scripts/ItemDropHandler.cs
+++ GraKulka/Assets/Scripts/ItemDropHandler.cs
@@ -26,18 +26,18 @@ public class ItemDropHandler : MonoBehaviour, IDropHandler
 				if (!Physics.Raycast(checkZ, 0.9f))
 				{
 
-					if (theitem.name == "Ramp")
+					if (theitem.name == "rampUp")
 					{
-						GameObject newPlaceAbleItem = Instantiate(Resources.Load("Prefabs/" + theitem.name), new Vector3(hit.collider.bounds.center.x - 0.5f, hit.point.y, hit.collider.bounds.center.z + 0.5f), Quaternion.Euler(0, 90, 0)) as GameObject;
+						GameObject newPlaceAbleItem = Instantiate(Resources.Load("Prefabs/" + theitem.name), new Vector3(hit.collider.bounds.center.x + 0.5f, hit.point.y + 1, hit.collider.bounds.center.z - 0.5f), Quaternion.Euler(180, 180, 0)) as GameObject;
 						newPlaceAbleItem.name = theitem.name;
 						gamestate.SavePlaceableItem(newPlaceAbleItem);
 						gamestate.EnableUndoButton();
 						theitem.parent.GetComponent<Button>().interactable = false;
 						theitem.GetComponent<ItemDragHandler>().IsDragAble = false;
 					}
-					if (theitem.name == "270Ramp")
+					if (theitem.name == "rampRight")
 					{
-						GameObject newPlaceAbleItem = Instantiate(Resources.Load("Prefabs/" + theitem.name), new Vector3(hit.collider.bounds.center.x + 0.5f, hit.point.y, hit.collider.bounds.center.z - 0.5f), Quaternion.Euler(0, 270, 0)) as GameObject;
+						GameObject newPlaceAbleItem = Instantiate(Resources.Load("Prefabs/" + theitem.name), new Vector3(hit.collider.bounds.center.x -0.5f, hit.point.y, hit.collider.bounds.center.z + 0.5f), Quaternion.Euler(0, 90, 0)) as GameObject;
 						newPlaceAbleItem.name = theitem.name;
 						gamestate.SavePlaceableItem(newPlaceAbleItem);
 						gamestate.EnableUndoButton();
