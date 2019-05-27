dd2349bbc18483e6d120b0e20fde65bb50900ddf
Sweetashne <mario2114@gmail.com>
2019-05-27 15:26:52 +0200

    Added two levels

--- GraKulka/Assets/Scripts/EndTrigger.cs
+++ GraKulka/Assets/Scripts/EndTrigger.cs
@@ -10,13 +10,37 @@ namespace Assets.Scripts
 
 		private void OnTriggerEnter(Collider other)
 		{
-			LevelComplete.SetActive(true);
-			Invoke("NextScene", delay);
+			if (other.name == "Player")
+			{
+				GetComponentInParent<Animator>().SetTrigger("IsTriggered");
+
+				if (other.GetComponent<Rigidbody>().velocity.z > 0.5f)
+				{
+					other.GetComponent<Rigidbody>().velocity = other.GetComponent<Rigidbody>().velocity - new Vector3(0, 0, 0.5f);
+					other.GetComponent<Rigidbody>().angularVelocity = other.GetComponent<Rigidbody>().angularVelocity - new Vector3(0, 0, 0.5f);
+					Invoke("LevelCompleted", 2);
+				}
+
+				if (other.GetComponent<Rigidbody>().velocity.x > 0.5f)
+				{
+					other.GetComponent<Rigidbody>().velocity = other.GetComponent<Rigidbody>().velocity - new Vector3(0.5f, 0, 0);
+					other.GetComponent<Rigidbody>().angularVelocity = other.GetComponent<Rigidbody>().angularVelocity - new Vector3(0.5f, 0, 0);
+					Invoke("LevelCompleted", 2);
+				}
+
+				
+				Invoke("NextScene", 2);
+			}
 		}
 
 		private void NextScene()
 		{
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+
+		private void LevelCompleted()
+		{
+			LevelComplete.SetActive(true);
 		}
 	}
 }
